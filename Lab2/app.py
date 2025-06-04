f30ca86f95eae16a111cbb736e6abf5fcf37d97d
unknown <sang09114@gmail.com>
2025-06-04 16:03:43 +0700

    update tempt

--- Lab2/app.py
+++ Lab2/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, json
 from cipher.caesar import CaesarCipher
+from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 
 app = Flask(__name__)
 
@@ -25,6 +27,46 @@ def caesar_decrypt():
         key = int(request.formp['inputCipherText'])
         Caesar = CaesarCipher()
         decrypted_text = Caesar.decrypt_text(text, key)
+        return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+@app.route("/vigenere")
+def vigenere():
+    return render_template('vigenere.html')
+
+@app.route("/encrypt", methods = ['POST'])
+def vigenere_encrypt():
+    text = request.form['inputPlainText']
+    key = int(request.form['inputKeyPlain'])
+    Vigenere = VigenereCipher()
+    encrypted_text = Vigenere.encrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route("/decrypt", methods = ['POST'])
+def vigenere_decrypt():
+        text = request.form['inputCipherText']
+        key = int(request.formp['inputCipherText'])
+        Vigenere = VigenereCipher()
+        decrypted_text = Vigenere.decrypt_text(text, key)
+        return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+@app.route("/railfence")
+def railfence():
+    return render_template('railfence.html')
+
+@app.route("/encrypt", methods = ['POST'])
+def railfence_encrypt():
+    text = request.form['inputPlainText']
+    key = int(request.form['inputKeyPlain'])
+    Railfence = RailFenceCipher()
+    encrypted_text = Railfence.encrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route("/decrypt", methods = ['POST'])
+def railfence_decrypt():
+        text = request.form['inputCipherText']
+        key = int(request.formp['inputCipherText'])
+        Railfence = RailFenceCipher()
+        decrypted_text = Railfence.decrypt_text(text, key)
         return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
 
 
